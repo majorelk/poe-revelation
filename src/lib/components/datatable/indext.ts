9001f319c56c5c59f96c40d5b95c236f137aab9e
majorelk <54996516+majorelk@users.noreply.github.com>
2025-01-02 14:30:24 +0000

    added easy ref export for all component values

---
+++ src/lib/components/datatable/indext.ts
@@ -0,0 +1,6 @@
+export { default as ThSort } from './ThSort.svelte';
+export { default as ThFilter } from './ThFilter.svelte';
+export { default as Search } from './Search.svelte';
+export { default as RowsPerPage } from './RowsPerPage.svelte';
+export { default as RowCount } from './RowCount.svelte';
+export { default as Pagination } from './Pagination.svelte';
